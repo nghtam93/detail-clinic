03438b1a27f4bde3b7a7f2ea1040e413d1262281
doannguyen263 <doannguyen263@gmail.com>
2022-04-08 22:15:30 +0700

    Update pc

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,19 @@
 jQuery(function($) {
     $(document).ready(function(){
 
+        $( ".main__nav li" ).hover(
+          function() {
+            $( '.main__nav' ).addClass('is-hover')
+          }, function() {
+            $( '.main__nav' ).removeClass('is-hover')
+          }
+        );
 
         new WOW().init();
         if($('body').hasClass( "dn-home" )){
 
         }
-        
+
 
     });
 });
